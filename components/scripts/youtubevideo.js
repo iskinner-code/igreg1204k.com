84934da2a6673e9c1df95b4304ab19fa6682e3bc
Ian Skinner <iansactivity@gmail.com>
2026-04-12 20:01:06 -0400

    updating some stuff

--- components/scripts/youtubevideo.js
+++ components/scripts/youtubevideo.js
@@ -14,18 +14,13 @@ async function fetchLatestVideo() {
     if (data.status === "ok" && data.items.length > 0) {
       // Pulls the very first item in the feed
       const latestVideo = data.items[0];
-      const videoLink = latestVideo.link;
-      const videoId =
-        latestVideo.guid.split(":")[2] || latestVideo.link.split("v=")[1];
-
-      const thumbnail = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
 
       // Only injecting the linked image
       container.innerHTML = `
         <div class="video-preview">
-          <a href="${videoLink}" target="_blank" rel="noopener">
+          <a href="${latestVideo.link}" target="_blank" rel="noopener">
             <div class="thumb-container">
-               <img src="${thumbnail}" alt="Latest Video">
+               <img src="${latestVideo.thumbnail}" alt="Latest Video">
                <div class="play-overlay">▶</div>
             </div>
           </a>
